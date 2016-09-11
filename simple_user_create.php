df1327605deb4c30dd86e6084a45364bd893122e
Joe Bacal <bacalj@gmail.com>
2016-09-11 16:21:04 -0400

    adds domain parameter

--- simple_user_create.php
+++ simple_user_create.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Quick Create User
-Description: Create a shib-compatible user ahead of time
+Description: Create a shib-compatible user ahead of time, and a portfolio for them if desired
 Author: Joe Bacal
 Version: 0.1
 */
@@ -15,6 +15,7 @@ function qcu_setup_menu(){
 }
 
 function qcu_form(){
+
   ?>
     <style>
       #qcu-form input {
@@ -45,6 +46,9 @@ function qcu_form(){
         <label for="qcu-setup-new-site">Set up a new Portfolio for this user?</label>
         <input type="checkbox" name="qcu-setup-new-site" value=""><br><br>
 
+        <label for="qcu-setup-domain">Domain for site:<br><em>If you are not at Smith College, you'll want to change this</em></label>
+        <input type="text" name="qcu-setup-domain" value="sophia.smith.edu"><br><br>
+
         <input type="submit" name="submit" class="button button-primary" value="Create Shibboleth compatible user">
       </form>
     </div>
@@ -57,9 +61,11 @@ function qcu_create_user() {
   //make sure no existe ya
  	if ((null == username_exists($_POST['qcu-email'])) && ( null == email_exists( $_POST['qcu-email']))) {
 
+    $domain   = sanitize_text_field($_POST['qcu-setup-domain']);
     $username = sanitize_text_field($_POST['qcu-email']);
-    $first = sanitize_text_field($_POST['qcu-first-name']);
-    $last = sanitize_text_field($_POST['qcu-last-name']);
+    $first    = sanitize_text_field($_POST['qcu-first-name']);
+    $last     = sanitize_text_field($_POST['qcu-last-name']);
+
     //set up the new users stuff
     $userdata = array(
  			'user_login'  => $username,
@@ -68,7 +74,7 @@ function qcu_create_user() {
  			'last_name'   => $last
  		);
 
- 		//create the new user
+ 		//create the new user and collect the ID
  		$user_id = wp_insert_user( $userdata );
 
     //flag for shibboleth
@@ -77,8 +83,6 @@ function qcu_create_user() {
 
     if ($_POST['qcu-setup-new-site'] == true ){
       //set up new site data
-      //TODO: make this selectable
-      $domain = 'sophia.smith.edu',
       $path = '/' .  substr($email, 0, strpos($email, '@')) . '/';
       $title = $first . ' ' . $last;
 
